57ef4a148847e625e4f295a6657f3615e1e0a752
RasulArslanov <rasularslanoov@gmail.com>
2020-06-19 06:38:32 +0500

    Implemented SongRepository and Flusher

--- music/src/Model/Flusher.php
+++ music/src/Model/Flusher.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class Flusher
 {
+    private EntityManagerInterface $em;
+
+    /**
+     * Flusher constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function flush(): void
     {
-
+        $this->em->flush();
     }
 }
--- music/src/Model/Music/Entity/Song/SongRepository.php
+++ music/src/Model/Music/Entity/Song/SongRepository.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace App\Model\Music\Entity\Song;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectRepository;
+
 class SongRepository
 {
+    private EntityManagerInterface $em;
+    private ObjectRepository $repo;
+
+    /**
+     * SongRepository constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(Song::class);
+    }
+
     public function add(Song $song): void
     {
-
+        $this->em->persist($song);
     }
 }
