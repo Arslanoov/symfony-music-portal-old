1d4e3838819647b1938b4dab5c702a1fcf61e776
RasulArslanov <rasularslanoov@gmail.com>
2020-06-22 20:10:25 +0500

    Installed Encore

--- music/config/bundles.php
+++ music/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
