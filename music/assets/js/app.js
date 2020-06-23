5244f7f405620ea8a6c2a649ea7b4421b303fa40
RasulArslanov <rasularslanoov@gmail.com>
2020-06-23 13:48:41 +0500

    Installed AdminLTE

--- music/assets/js/app.js
+++ music/assets/js/app.js
@@ -1,3 +1,15 @@
 require('../scss/app.scss');
 
 require('bootstrap');
+
+require('jquery');
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+require('admin-lte');
+require('admin-lte/plugins/fontawesome-free/css/all.min.css');
+require('admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css');
+require('admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min');
+require('admin-lte/plugins/overlayScrollbars/js/OverlayScrollbars.min');
+
