9d1efd69408ec005bf1a542dfd542026c1f8441c
RasulArslanov <rasularslanoov@gmail.com>
2020-06-25 12:53:40 +0500

    Revert "Added genre manage and role confines"
    This reverts commit ee1ef20998c310727749c0fc2b4e4b76b0e9644f.

--- music/src/Http/Controller/Web/Manage/Music/GenreController.php
+++ music/src/Http/Controller/Web/Manage/Music/GenreController.php
@@ -6,11 +6,8 @@ namespace App\Http\Controller\Web\Manage\Music;
 
 use App\Http\Controller\Web\BaseController;
 use App\ReadModel\Music\Genre\GenreFetcher;
-use DomainException;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Model\Music\UseCase\Genre;
 
 /**
  * Class GenreController
@@ -29,23 +26,5 @@ class GenreController extends BaseController
         $genres = $genreFetcher->all();
 
         return $this->render('manage/music/genre/home.html.twig', compact('genres'));
-    }
-
-    public function create(Request $request, Genre\Create\Handler $handler): Response
-    {
-        $command = new Genre\Create\Command();
-
-        $form = $this->createForm(Genre\Create\Form::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() and $form->isValid()) {
-            try {
-                $handler->handle($command);
-                $this->addFlash('success', 'Genre successfully created');
-                return $this->redirectToRoute('manage.genres');
-            } catch (DomainException $e) {
-                
-            }
-        }
     }
 }
